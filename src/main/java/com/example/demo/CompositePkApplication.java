411918c271c8fd450098252aeb901158bdcbac3a
parallels <parallels@ubuntu>
2019-02-11 22:08:28 -0800

    Initial Commit

---
+++ src/main/java/com/example/demo/CompositePkApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.example.demo.model.Person;
+import com.example.demo.model.PersonPK;
+import com.example.demo.service.PersonService;
+
+@SpringBootApplication
+@EntityScan("com.example.demo.model")
+@EnableJpaRepositories(basePackages= {"com.example.demo.repository"})
+
+public class CompositePkApplication  implements CommandLineRunner{
+    
+	@Autowired
+	PersonService ps;
+	
+	@Value("${spring.datasource.username}")
+	String userName;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CompositePkApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Person p = ps.save("John Doe",2,32);
+		System.out.println("Person inserted with fullname "+p.getPersonPK().getFullname()+" db user "+userName);
+		
+		Optional<Person> p2 = ps.getByPK(new PersonPK("John Doe",2));
+		System.out.println("Person retrieved with fullname "+p2.get().getPersonPK().getFullname());
+	}
+}
